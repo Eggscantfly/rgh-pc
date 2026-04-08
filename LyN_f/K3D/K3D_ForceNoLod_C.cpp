934305f70a17163d39f5073c3f416d0f0764f356
Eggscantfly <103383950+Eggscantfly@users.noreply.github.com>
2026-04-08 04:18:41 -0500

    Update K3D_ForceNoLod_C.cpp

--- LyN_f/K3D/K3D_ForceNoLod_C.cpp
+++ LyN_f/K3D/K3D_ForceNoLod_C.cpp
@@ -1,4 +1,4 @@
-void __cdecl K3D_ForceNoLod_C(long param_1)
+void __cdecl K3D_ForceNoLod_C(long ForcedLOD)
 {
-    *(long*)((char*)K3D_gpo_Display + 0x69410) = param_1;
+    *(long*)((char*)K3D_gpo_Display + 0x69410) = ForcedLOD;
 }
